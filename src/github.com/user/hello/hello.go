b089c95f48b0097d837e30b6f80b722af8000642
freeman <liuxiong21@gmail.com>
2015-04-08 15:53:21 +0800

    create new go project

---
+++ src/github.com/user/hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"github.com/user/stringutil"
+)
+
+func main() {
+	fmt.Printf("Hello, world.\n")
+	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	fmt.Println("update")
+}
+
+
