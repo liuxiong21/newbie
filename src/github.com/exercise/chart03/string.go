4d892b383100aa15a17310608aa65fadcf795ad3
freeman <liuxiong21@gmail.com>
2015-04-17 23:52:22 +0800

    add

--- src/github.com/exercise/chart03/string.go
+++ src/github.com/exercise/chart03/string.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
+	"unicode/utf8"
 )
 
 func main() {
@@ -16,8 +18,34 @@ func main() {
 		æs += string(char)
 	}
 	text := "中 国人 I am chinese"
-	firstCharEndIndex := strings.Index(text," ")
+	firstCharEndIndex := strings.Index(text, " ")
 	firstChar := text[:firstCharEndIndex]
 	fmt.Println()
 	fmt.Println(firstChar)
+	Humanize(10000000.299, 30, 10, "_","0")
+}
+
+func Humanize(amount float64, width, decimals int, sp,pad string) {
+	dollars, cents := math.Modf(amount)
+	whole := fmt.Sprintf("%+.0f", dollars)[1:]
+	fractions := ""
+	if decimals > 0 {
+		fractions = fmt.Sprintf("%+.*f", decimals, cents)[2:]
+	}
+	for i := len(whole) - 3; i > 0; i = i - 3 {
+		whole = whole[:i] + sp + whole[i:]
+	}
+	for i := 4; i < len(fractions); i = i + 3 {
+		fractions = fractions[:i] + sp + fractions[i:]
+		i++
+	}
+	number := whole + fractions
+	if amount<0.0{
+		number = "-"+number
+	}
+	gap := width - utf8.RuneCountInString(number)
+	if gap>0{
+		number = strings.Repeat(pad,gap)+number
+	}
+	fmt.Println(number)
 }
