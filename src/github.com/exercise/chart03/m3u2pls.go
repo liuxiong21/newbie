ddf1a5a1d956b0ad53f9866f764a59c973182bc9
freeman <liuxiong21@gmail.com>
2015-04-19 20:10:07 +0800

    add

--- src/github.com/exercise/chart03/m3u2pls.go
+++ src/github.com/exercise/chart03/m3u2pls.go
@@ -1,13 +1,15 @@
 package main
 
 import (
-	"os"
-	"strings"
-	"log"
-	"io"
-	"fmt"
-	"path/filepath"
 	"bufio"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+	"fmt"
+	"strconv"
 )
 
 /**
@@ -27,42 +29,61 @@ Music/David Bowie/Singles 1/01-Space Oddity.ogg
 
 */
 
-func M3u2pls(source,target string){
-	if strings.TrimSpace(source)==""{
+type Song struct{
+	title string
+	filepath string
+	length int
+}
+
+func M3u2pls(source, target string) {
+	if strings.TrimSpace(source) == "" {
 		log.Fatal("Source file path is invalid")
 	}
-	
-	sourceFile,err := os.Open(source)
+
+	sourceFile, err := os.Open(source)
 	defer sourceFile.Close()
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
-	err = os.MkdirAll(filepath.Dir(target),0777)
-	if err!=nil{
+	err = os.MkdirAll(filepath.Dir(target), 0777)
+	if err != nil {
 		panic(err)
 	}
-	targetFile,err := os.Create(target)
-	if err!=nil{
+	targetFile, err := os.Create(target)
+	if err != nil {
 		panic(err)
 	}
 	reader := bufio.NewReader(sourceFile)
 	writer := bufio.NewWriter(targetFile)
 	defer writer.Flush()
+	titleRex := regexp.MustCompile(`^#EXTINF\s*:\s*(?P<length>\d+),(?P<title>.+)`)
+	var counter int
 	for {
-		line,err := reader.ReadBytes('\n')
-		if err!=nil{
-			if err== io.EOF{
+		line, err := reader.ReadBytes('\n')
+		if err != nil {
+			if err == io.EOF {
 				break
 			}
 			panic(err)
 		}
 		lineStr := string(line[:len(line)-1])
-		if lineStr=="#EXTM3U"{
+		if lineStr == "#EXTM3U" {
 			writer.WriteString("[playlist]\n")
+		} else if titleRex.MatchString(lineStr) {
+			song := Song{}
+			groups := titleRex.FindAllStringSubmatch(lineStr, -1)
+			for i := 0; i < len(groups); i++ {
+				song.length,err = strconv.Atoi(groups[i][1])
+				song.title = groups[i][2]
+				line,err = reader.ReadBytes('\n')
+				song.filepath = string(line[:len(line)-1])
+				counter++
+				writer.WriteString(fmt.Sprintf("File%d=%s\n",counter,song.filepath))
+				writer.WriteString(fmt.Sprintf("Title%d=%s\n",counter,song.title))
+				writer.WriteString(fmt.Sprintf("Length%d=%d\n",counter,song.length))
+			}
 		}
-		fmt.Println(lineStr)
 	}
+	writer.WriteString(fmt.Sprintf("NumberOfEntries=%d\n",counter))
+	writer.WriteString("Version=2")
 }
-
-
-
