78424257c8d39bd4a6800e3d16240db760d3c9a8
freeman <liuxiong21@gmail.com>
2015-05-01 10:37:16 +0800

    add

--- src/github.com/exercise/chart06/chart06.go
+++ src/github.com/exercise/chart06/chart06.go
@@ -1,4 +1,4 @@
-package main 
+package main
 
 import (
 	"fmt"
@@ -6,47 +6,48 @@ import (
 )
 
 func main() {
-	var phrases []string = []string{"Day; dusk, and light.","All day long"}
+	var phrases []string = []string{"Day; dusk, and light.", "All day long"}
 	processing(phrases)
-	fmt.Printf("%v\n",phrases)
+	fmt.Printf("%v\n", phrases)
 	var counter Counter
 	counter.Increment()
 	fmt.Println(counter)
 	counter.Increment()
 	fmt.Println(counter)
 	fmt.Println(counter.IsZero())
-	part := Part{11,"liuxiong"}
+	part := Part{11, "liuxiong"}
 	fmt.Println(part)
-	item := Item{"001",10.9,10}
+	item := Item{"001", 10.9, 10}
 	fmt.Println(item.Cost())
-	specialItem := SpecialItem{Item{"001",10.9,10},5}
+	specialItem := SpecialItem{Item{"001", 10.9, 10}, 5}
 	fmt.Println(specialItem.Cost())
 	costLiteral := (*SpecialItem).Cost
 	fmt.Println(costLiteral(&specialItem))
-	stringPair := StringPair{"This is First","This is Second"}
+	stringPair := StringPair{"This is First", "This is Second"}
 	fmt.Println(stringPair)
 	exchange(&stringPair)
 	fmt.Println(stringPair)
-	datas := make([]byte,100)
+	datas := make([]byte, 100)
 	var reader io.Reader = &stringPair
-	size,err := reader.Read(datas)
-	if err!=nil{
+	size, err := reader.Read(datas)
+	if err != nil {
 		fmt.Println(err)
-	}else{
-		fmt.Printf("%q\n",datas[:size])
+	} else {
+		fmt.Printf("%q\n", datas[:size])
 	}
 	fmt.Println(stringPair)
-	bytes := make([]byte,0)
-	bytes = append(bytes,'d')
-	fmt.Printf("%q\n",bytes)
-	part2 := Part{12,"thE case-insensible"}
+	bytes := make([]byte, 0)
+	bytes = append(bytes, 'd')
+	fmt.Printf("%q\n", bytes)
+	part2 := Part{12, "thE case-insensible"}
 	changePartCase(&part2)
 	printPerson()
 	printAuthor1()
 	printAuthor2()
+	printOptions()
 }
 
-func exchange(function Exchanger){
+func exchange(function Exchanger) {
 	function.Exchange()
 }
 
--- src/github.com/exercise/chart06/structs.go
+++ src/github.com/exercise/chart06/structs.go
@@ -51,3 +51,73 @@ func printAuthor2(){
 	author2 := Author2{person1,"this is title",1950} 
 	fmt.Println(author2)	
 }
+
+type Optioner interface {
+	Name() string
+	IsValid() bool
+}
+
+type OptionCommon struct {
+	ShortName string "this Short Name"
+	LongName  string "this is Long Name"
+}
+
+type IntOption struct {
+	OptionCommon
+	Value, Max, Min int
+}
+
+func (option IntOption) Name() string {
+	return name(option.ShortName, option.LongName)
+}
+
+func (option IntOption) IsValid() bool {
+	return option.Value >= option.Min && option.Value <= option.Max
+}
+
+type FloatOption struct {
+	Optioner
+	Value float64
+}
+
+type StringOption struct{
+	Optioner
+	Value string
+}
+
+func (option FloatOption) Name() string {
+	return fmt.Sprintf("FloatOption[%v]", option.Value)
+}
+
+func (option FloatOption) IsValid() bool {
+	return true
+}
+
+type GenericOption struct {
+	OptionCommon
+}
+
+func (option GenericOption) Name() string {
+	return name(option.ShortName, option.LongName)
+}
+
+func (option GenericOption) IsValid() bool {
+	return true
+}
+
+func printOptions(){
+	fileOption := StringOption{GenericOption{OptionCommon{"My name is ShortName","My name is LongName"}},
+		"index.html"}
+	floatOption := FloatOption{Optioner:GenericOption{OptionCommon{"s1","l1"}},Value:19.9}
+	optioners := []Optioner{&fileOption,&floatOption}
+	for _,op := range optioners{
+		fmt.Println(op.Name(),"---",op.IsValid())
+	}
+}
+
+func name(shortName, longName string) string {
+	if shortName == "" {
+		return shortName
+	}
+	return longName
+}
