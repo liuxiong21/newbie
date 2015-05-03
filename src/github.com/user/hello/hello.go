e1f5dde5f77b335d37b2760e68f24e5f4dec026f
freeman <liuxiong21@gmail.com>
2015-05-03 20:53:37 +0800

    add

--- src/github.com/user/hello/hello.go
+++ src/github.com/user/hello/hello.go
@@ -2,10 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/exercise/chart06/omap"
 	"github.com/user/stringutil"
 )
 
 func main() {
 	fmt.Printf("Hello, world.\n")
 	fmt.Printf(stringutil.Reverse("!oG ,olleH"))
+	sortMap := omap.NewOmap(func(first, second interface{})bool {
+			switch first.(type){
+				case int,int32,int64:
+					if first.(int64) < second.(int64){
+						return true
+					}
+			}
+			
+			return false
+	})
+	fmt.Println("\n",sortMap.Len())
 }
