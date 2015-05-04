493e4a3f30056070222d7787b003b3b01eb4d8cf
freeman <liuxiong21@gmail.com>
2015-05-04 23:02:00 +0800

    ad

---
+++ src/github.com/exercise/chart07/parallel_jobs.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+type Job struct{
+	Time string
+	Name string
+}
+
+func RunParallelJobs(){
+	jobList := []*Job{&Job{"2015-05-03 09:00:00","send mail"},
+		&Job{"2015-05-03 19:00:00","compute access convert rate"}}
+	done := make(chan bool,len(jobList))
+	jobs := make(chan *Job)
+	go func(){
+		for _,job:= range jobList{
+			jobs <- job
+			fmt.Println("Push new Job to channel")
+		}
+		close(jobs)
+	}()
+	go func(){
+		for job:= range jobs{
+			time.Sleep(time.Second*time.Duration(rand.Intn(10)))
+			fmt.Printf("%s job handle finished start at %s\n",job.Name,job.Time)
+			done <- true
+		}
+	}()
+	for i:=0;i<len(jobList);i++{
+		<-done
+	}
+}
+
